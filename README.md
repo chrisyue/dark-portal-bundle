e629b1c47ce410c10185f6557e6078f5354f1cfd
chrisyue <blizzchris@gmail.com>
2015-06-13 22:37:41 +0800

    update readme and get-code.php

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 OAuth 2.0 登录助手
 ==================
 
-此库是为了解决OAuth2.0登录时多个域名使用同一个OAuth2.0服务提供账号登录的需求
-
-比如wechat.readtv.cn和jl.promo.readtv.cn都需要使用同一个微信公众账号登录，公众账号只能设置一个redirect_uri的域名。
+某些OAuth2.0用户提供商，可能要求只能设置一个redirect_uri host，但是你却想使用多个redirect_uri host，如果是这种情况，此库适合你。
 
 原理
 ----
@@ -12,18 +10,20 @@ OAuth 2.0 登录助手
 
 只有在获取code步骤，公众号设置的redirect_uri才用作用（限制使用公众号的域名的作用）
 
+而获取access token 阶段完全没有此限制，直接使用code获取就行了
+
 使用方式
 --------
 
-将一台服务器设置为专门的获取code的服务器，比如：oauth-code.readtv.cn
+比如wechat.xxx.com和weixin.xxx.com都需要使用同一个微信公众账号登录，公众账号只能设置一个redirect_uri的域名。
+
+将一台服务器设置为专门的获取code的服务器，比如：oauth-code.xxx.com
 
 将get-code.php代码部署到服务器，部署前可设置能使用此服务的域名：
 
 ```php
-$sites = [
-    'wechat.readtv.cn',
-    'jl.promo.readtv.cn',
+$hosts = [
+    'wechat.xxx.com',
+    'weixin.xxx.com',
 ];
 ```
-
-WIP
---
+++ get-code.php
@@ -0,0 +1,53 @@
+<?php
+
+if (empty($_GET['redirect_uri']) && empty($_GET['code'])) {
+    header('status: 422'); die('<h1>:(</h1>');
+}
+
+session_start();
+
+if (!empty($_GET['code'])) {
+    if (empty($_SESSION['redirect_uri'])) {
+        header('status: 422'); die('<h1>:(</h1>');
+    }
+
+    $parts = parse_url($_SESSION['redirect_uri']);
+    session_unset();
+
+    parse_str($parts['query'], $vars);
+    $vars['code'] = $_GET['code'];
+
+    $redirectUri = sprintf('%s://%s%s%s?%s',
+        $parts['scheme'], $parts['host'],
+        isset($parts['port']) ? sprintf(':%s', $parts['port']) : '',
+        $parts['path'],
+        http_build_query($vars)
+    );
+
+    header(sprintf('Location: %s', $redirectUri)); die();
+}
+
+$hosts = [
+    'wechat.xxx.com',
+    'weixin.xxx.com',
+];
+
+$parts = parse_url($_GET['redirect_uri']);
+
+if (!in_array($parts['host'], $hosts)) {
+    header('status: 422'); die('<h1>:(</h1>');
+}
+
+$_SESSION['redirect_uri'] = $_GET['redirect_uri'];
+
+$queries = [
+    'appid' => 'wxdcba7b97117e6d0a',
+    // temporary solution, not reliable (no scheme and uri check)
+    'redirect_uri' => sprintf('http://%s', $_SERVER['HTTP_HOST']),
+    'response_type' => 'code',
+    'scope' => empty($_GET['scope']) ? 'snsapi_base' : $_GET['scope'],
+];
+
+$urlGetCode = 'https://open.weixin.qq.com/connect/oauth2/authorize?'.http_build_query($queries).'#wechat_redirect';
+
+header(sprintf('Location: %s', $urlGetCode));
